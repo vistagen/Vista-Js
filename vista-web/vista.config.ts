b6674f03403872b361518fbc994407eee1c94d08
ANKAN DALUI <ankan.dalui.fiem.cse22@teamfuture.in>
2026-01-05 22:31:20 +0530

    feat: initial commit (clean)

---
+++ vista-web/vista.config.ts
@@ -0,0 +1,14 @@
+
+import type { VistaConfig } from 'vista/config';
+
+const config: VistaConfig = {
+    images: {
+        domains: ['example.com'],
+    },
+    // Optional: override server port
+    // server: {
+    //     port: 3000
+    // }
+};
+
+export default config;
